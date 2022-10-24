02310fc46dec133e9e4681e75f6969e5d5bdf114
AndreGP <andre18july@gmail.com>
2022-10-24 20:51:39 +0100

    implementando metodo para put de marcas update

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -52,7 +52,8 @@ class MarcaController extends Controller
      */
     public function update(Request $request, Marca $marca)
     {
-        //
+        $marca->update($request->all());
+        return $marca;
     }
 
     /**
